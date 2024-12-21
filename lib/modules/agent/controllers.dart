0ef64e16c59984acc5a14f6a2ef6c7db37af6db7
jamfaraz <farazj105@gmail.com>
2024-12-21 23:10:12 +0500

    agent acount creatd

--- lib/modules/agent/controllers.dart
+++ lib/modules/agent/controllers.dart
@@ -1,9 +1,87 @@
+import 'dart:io';
+import 'package:bibaho_sheba/routes/app_pages.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
+import 'package:file_picker/file_picker.dart';
 
 class CreateAgentController extends GetxController {
-  static CreateAgentController get instanse => CreateAgentController();
-
+  final TextEditingController firstNameController = TextEditingController();
+  final TextEditingController lastNameController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
-  final TextEditingController passController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  RxBool isLoading = false.obs;
+  RxBool isChecked = false.obs;
+  Rx<File?> selectedFile = Rx<File?>(null);
+
+  Future<void> selectFile() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+    if (result != null && result.files.single.path != null) {
+      selectedFile.value = File(result.files.single.path!);
+      Get.snackbar('File Selected', 'File: ${result.files.single.name}',
+          backgroundColor: Colors.green, colorText: Colors.white);
+    } else {
+      Get.snackbar('Error', 'No file selected',
+          backgroundColor: Colors.red, colorText: Colors.white);
+    }
+  }
+
+  Future<void> createAgent() async {
+    if (emailController.text.isEmpty ||
+        passwordController.text.isEmpty ||
+        firstNameController.text.isEmpty ||
+        lastNameController.text.isEmpty ||
+        phoneController.text.isEmpty 
+        // ||
+        // selectedFile.value == null
+        ) {
+      Get.snackbar('Error', 'All fields are required',
+          backgroundColor: Colors.red, colorText: Colors.white);
+      return;
+    }
+
+    isLoading.value = true;
+
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+              email: emailController.text.trim(),
+              password: passwordController.text.trim());
+
+      await FirebaseFirestore.instance
+          .collection('agents')
+          .doc(userCredential.user!.uid)
+          .set({
+        'firstName': firstNameController.text.trim(),
+        'lastName': lastNameController.text.trim(),
+        'email': emailController.text.trim(),
+        'phone': phoneController.text.trim(),
+        'fileName': selectedFile.value?.path.split('/').last,
+        'createdAt': FieldValue.serverTimestamp(),
+      });
+
+      Get.snackbar('Success', 'Agent account created successfully',
+          colorText: Colors.white, backgroundColor: Colors.green);
+
+      firstNameController.clear();
+      lastNameController.clear();
+      emailController.clear();
+      phoneController.clear();
+      passwordController.clear();
+      selectedFile.value = null;
+      isChecked.value = false;
+
+      Get.offAllNamed(Routes.AGENT_DASHBOARD);
+    } catch (e) {
+      Get.snackbar('Error', e.toString(),
+          snackPosition: SnackPosition.BOTTOM,
+          backgroundColor: Colors.red,
+          colorText: Colors.white);
+    } finally {
+      isLoading.value = false;
+    }
+  }
 }
--- lib/modules/agent/views.dart
+++ lib/modules/agent/views.dart
@@ -10,6 +10,179 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 
+// class CreateAgentView extends GetView<CreateAgentController> {
+//   const CreateAgentView({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+// appBar: AppBar(
+//   backgroundColor: AppColors.primaryColor,
+//   centerTitle: true,
+//   title: Text(
+//     'Agent',
+//     style: Theme.of(context)
+//         .textTheme
+//         .titleLarge!
+//         .copyWith(color: Colors.white),
+//   ),
+//   leading: IconButton(
+//       onPressed: Get.back,
+//       icon: const Icon(
+//         Icons.arrow_back,
+//         color: Colors.white,
+//       )),
+// ),
+//       body: SingleChildScrollView(
+//         child: Padding(
+//           padding: const EdgeInsets.symmetric(horizontal: 20.0),
+//           child: Column(
+//             children: [
+//               const SizedBox(
+//                 height: 12,
+//               ),
+//               SizedBox(
+//                   height: AppSizes.width / 3,
+//                   width: AppSizes.width,
+//                   child: Image(image: AssetImage(AppImages.appLogo))),
+//               const CustomBibahoShebaText(),
+//               const SizedBox(
+//                 height: 20,
+//               ),
+//               Row(
+//                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                 children: [
+//                   SizedBox(
+//                     width: AppSizes.width / 2.8,
+//                     child: TextFormField(
+//                       controller: controller.firstNameController,
+//                       decoration: const InputDecoration(
+//                         hintText: "First Name",
+//                       ),
+//                     ),
+//                   ),
+//                   SizedBox(
+//                     width: AppSizes.width / 2.8,
+//                     child: TextFormField(
+//                       controller: controller.lastNameController,
+//                       decoration: const InputDecoration(
+//                         hintText: "Last Name",
+//                       ),
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//               SizedBox(
+//                 height: 20,
+//               ),
+//               TextFormField(
+//                 controller: controller.emailController,
+//                 decoration: const InputDecoration(
+//                   hintText: "Enter New Email",
+//                 ),
+//               ),
+//               const SizedBox(
+//                 height: 20,
+//               ),
+//               TextFormField(
+//                   controller: controller.phoneController,
+//                   decoration: InputDecoration(
+//                     hintText: "Enter Phone Number",
+//                   )),
+//               const SizedBox(
+//                 height: 20,
+//               ),
+//               TextFormField(
+//                   controller: controller.passwordController,
+//                   decoration: InputDecoration(
+//                     hintText: "Enter Password",
+//                   )),
+//               SizedBox(
+//                 height: 20,
+//               ),
+//               Row(
+//                 children: [
+//                   Icon(
+//                     Icons.star,
+//                     size: 10,
+//                   ),
+//                   Text(
+//                     'Upload your personal document for verification',
+//                     style: Theme.of(context)
+//                         .textTheme
+//                         .bodySmall!
+//                         .copyWith(color: Colors.grey),
+//                   ),
+//                 ],
+//               ),
+//               Container(
+//                 height: 30,
+//                 decoration: BoxDecoration(
+//                     border: Border.all(color: Colors.grey),
+//                     color: Colors.grey[200],
+//                     borderRadius: BorderRadius.circular(5)),
+//                 child: Row(
+//                   mainAxisAlignment: MainAxisAlignment.spaceAround,
+//                   children: [Text('Choose File'), Icon(Iconsax.arrow_down_1)],
+//                 ),
+//               ),
+//               SizedBox(
+//                 height: 20,
+//               ),
+//               Row(
+//                 children: [
+//                   Checkbox(
+//                       activeColor: AppColors.secondaryColor,
+//                       value: true,
+//                       onChanged: (value) {},
+//                       checkColor: Colors.white,
+//                       side: const BorderSide(color: Colors.grey)),
+//                   Column(
+//                     crossAxisAlignment: CrossAxisAlignment.start,
+//                     children: [
+//                       Text(
+//                         softWrap: true,
+//                         maxLines: 2,
+//                         "I agree to Bibaho Sheba User Agreement &",
+//                         style: Theme.of(context).textTheme.bodySmall,
+//                       ),
+//                       Text(
+//                         softWrap: true,
+//                         maxLines: 2,
+//                         "Privacy Policy",
+//                         style: Theme.of(context)
+//                             .textTheme
+//                             .bodySmall!
+//                             .copyWith(color: AppColors.primaryColor),
+//                       ),
+//                     ],
+//                   )
+//                 ],
+//               ),
+//               SizedBox(
+//                 height: 20,
+//               ),
+//         Obx(  () => controller.isLoading.value
+//             ? const Center(child: CircularProgressIndicator(
+
+//             ))
+//             :    CustomButton(
+//                 text: "Continue",
+//                 ontap: () {
+//                   controller.createAgent();
+//                   // Get.toNamed(Routes.AGENT_DASHBOARD);
+//                 },
+//                 height: 45,
+//                 width: AppSizes.width,
+//               ))
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 class CreateAgentView extends GetView<CreateAgentController> {
   const CreateAgentView({super.key});
 
@@ -39,7 +212,7 @@ class CreateAgentView extends GetView<CreateAgentController> {
           child: Column(
             children: [
               const SizedBox(
-                height: 20,
+                height: 6,
               ),
               SizedBox(
                   height: AppSizes.width / 3,
@@ -55,121 +228,114 @@ class CreateAgentView extends GetView<CreateAgentController> {
                   SizedBox(
                     width: AppSizes.width / 2.8,
                     child: TextFormField(
-                      controller: controller.emailController,
+                      controller: controller.firstNameController,
                       decoration: const InputDecoration(
-                        hintText: "First Name",
-                      ),
+                          hintText: "First Name", isDense: true),
                     ),
                   ),
                   SizedBox(
                     width: AppSizes.width / 2.8,
                     child: TextFormField(
-                      controller: controller.emailController,
+                      controller: controller.lastNameController,
                       decoration: const InputDecoration(
+                        isDense: true,
                         hintText: "Last Name",
                       ),
                     ),
                   ),
                 ],
               ),
-              SizedBox(
-                height: 20,
-              ),
+              const SizedBox(height: 20),
               TextFormField(
                 controller: controller.emailController,
-                decoration: const InputDecoration(
-                  hintText: "Enter New Email",
-                ),
+                decoration:
+                    const InputDecoration(hintText: "Email", isDense: true),
               ),
-              const SizedBox(
-                height: 20,
-              ),
+              const SizedBox(height: 20),
               TextFormField(
-                  controller: controller.passController,
-                  decoration: InputDecoration(
-                    hintText: "Enter Phone Number",
-                  )),
-              const SizedBox(
-                height: 20,
+                controller: controller.phoneController,
+                decoration:
+                    const InputDecoration(hintText: "Phone", isDense: true),
               ),
+              const SizedBox(height: 20),
               TextFormField(
-                  controller: controller.passController,
-                  decoration: InputDecoration(
-                    hintText: "Enter Password",
-                  )),
-              SizedBox(
-                height: 20,
+                controller: controller.passwordController,
+                decoration:
+                    const InputDecoration(hintText: "Password", isDense: true),
               ),
-              Row(
-                children: [
-                  Icon(
-                    Icons.star,
-                    size: 10,
-                  ),
-                  Text(
-                    'Upload your personal document for verification',
-                    style: Theme.of(context)
-                        .textTheme
-                        .bodySmall!
-                        .copyWith(color: Colors.grey),
-                  ),
-                ],
-              ),
-              Container(
-                height: 30,
-                decoration: BoxDecoration(
+              const SizedBox(height: 20),
+              GestureDetector(
+                onTap: controller.selectFile,
+                child: Container(
+                  height: 38,
+                  decoration: BoxDecoration(
                     border: Border.all(color: Colors.grey),
                     color: Colors.grey[200],
-                    borderRadius: BorderRadius.circular(5)),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [Text('Choose File'), Icon(Iconsax.arrow_down_1)],
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Obx(() => Text(
+                            controller.selectedFile.value == null
+                                ? 'Choose File'
+                                : controller.selectedFile.value!.path
+                                    .split('/')
+                                    .last,
+                          )),
+                      const Icon(Icons.attach_file),
+                    ],
+                  ),
                 ),
               ),
-              SizedBox(
-                height: 20,
-              ),
+              const SizedBox(height: 20),
               Row(
                 children: [
-                  Checkbox(
-                      activeColor: AppColors.secondaryColor,
-                      value: true,
-                      onChanged: (value) {},
-                      checkColor: Colors.white,
-                      side: const BorderSide(color: Colors.grey)),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        softWrap: true,
-                        maxLines: 2,
-                        "I agree to Bibaho Sheba User Agreement &",
-                        style: Theme.of(context).textTheme.bodySmall,
-                      ),
-                      Text(
-                        softWrap: true,
-                        maxLines: 2,
-                        "Privacy Policy",
-                        style: Theme.of(context)
-                            .textTheme
-                            .bodySmall!
-                            .copyWith(color: AppColors.primaryColor),
-                      ),
-                    ],
+                  Obx(
+                    () => Checkbox(
+                      activeColor: AppColors.primaryColor,
+                      value: controller.isChecked.value,
+                      onChanged: (value) {
+                        controller.isChecked.value = value!;
+                      },
+                    ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      "I agree to the terms and conditions",
+                      style: TextStyle(color: Colors.grey[700]),
+                    ),
                   )
                 ],
               ),
-              SizedBox(
-                height: 20,
-              ),
-              CustomButton(
-                text: "Continue",
-                ontap: () {
-                  Get.toNamed(Routes.AGENT_DASHBOARD);
-                },
-                height: 45,
-                width: AppSizes.width,
-              )
+              const SizedBox(height: 20),
+            
+              Obx(() => controller.isLoading.value
+                  ?  CircularProgressIndicator(color: AppColors.primaryColor,)
+                  : ElevatedButton(
+                    
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.all(10),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5))),
+                      onPressed: controller.isChecked.value
+                          ? controller.createAgent
+                          : null,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Continue",
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyLarge!
+                                .copyWith(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w500),
+                          ),
+                        ],
+                      ),
+                    )),
             ],
           ),
         ),
